867eb0b6ad36be269f03279858751f900751d41e
wang1zhen <wang1zhendire@hotmail.com>
2026-08-13 19:44:53 +0800

    update to 2512

--- update_feeds.sh
+++ update_feeds.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
+set -e
 
-rm -rf ./feeds/packages/lang/golang
+# Update all feeds and initially generate indexes
 ./scripts/feeds update -a
-rm -rf ./feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# Remove conflicting LuCI package
+rm -rf feeds/luci/applications/luci-app-mosdns
+
+# Remove packages that should be provided by kenzo/small
+rm -rf \
+    feeds/packages/net/alist \
+    feeds/packages/net/adguardhome \
+    feeds/packages/net/mosdns \
+    feeds/packages/net/xray* \
+    feeds/packages/net/v2ray* \
+    feeds/packages/net/sing* \
+    feeds/packages/net/smartdns \
+    feeds/packages/utils/v2dat \
+    feeds/packages/lang/golang
+
+# Replace Go toolchain
+git clone \
+    https://github.com/kenzok8/golang \
+    -b 1.26 \
+    feeds/packages/lang/golang
+
+# IMPORTANT: regenerate indexes after modifying feed contents
+./scripts/feeds update -i packages luci
+
+# Clean old feed symlinks and reinstall
+rm -rf package/feeds
 ./scripts/feeds install -a
